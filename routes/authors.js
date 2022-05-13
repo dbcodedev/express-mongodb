564fa1a490b4b05261d79c8d9c8775905863cce5
David B <davidb6343@gmail.com>
2022-05-13 17:01:30 +0200

    authors route + views

--- routes/authors.js
+++ routes/authors.js
@@ -1,16 +1,43 @@
 const express = require("express")
+const author = require("../models/author")
 const router = express.Router()
+const Author = require("../models/author")
 
-router.get("/", (req, res) => {
-    res.render("authors/index")
+router.get("/", async (req, res) => {
+    try {
+        let searchOptions = {}
+        if (req.query.name && req.query.name !== "") {
+            searchOptions.name = new RegExp(req.query.name, "i")
+        }
+        const authors = await Author.find(searchOptions)
+        res.render("authors/index", { authors, searchOptions: req.query })
+    } catch {
+        res.render("/", {
+            errorMessage: "Error getting authors"
+        })
+    }
 })
 
 router.get("/new", (req, res) => {
-    res.render("authors/new")
+    res.render("authors/new", {
+        author: new Author()
+    })
 })
 
-router.post("/", (req, res) => {
-    res.send("Create")
+router.post("/", async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save()
+        // res.redirect(`authors/${newAuthor.id}`)
+        res.redirect(`authors`)
+    } catch {
+        res.render("authors/new", {
+            author: author,
+            errorMessage: "Error creating author"
+        })
+    }
 })
 
 module.exports = router
